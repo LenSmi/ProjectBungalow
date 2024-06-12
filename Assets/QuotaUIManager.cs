655b918623faee2a28708525fb01abad3682c1fe
LennyEliasSmith <lenny.elias.s@gmail.com>
2024-06-12 08:59:09 +0300

    Worked on prefabs

---
+++ Assets/QuotaUIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG;
+
+public class QuotaUIManager : MonoBehaviour
+{
+    private MinigameManager minigameManager;
+    public Image fillImage;
+    public float quotaLerpValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minigameManager = GameManager.Instance().minigameManager();
+        MinigameManager.AddToDepositQuota += UpdateQuotaUI;
+        fillImage.fillAmount = minigameManager.StartingQuota / minigameManager.QuotaThreshold;
+    }
+
+    private void UpdateQuotaUI()
+    {
+        int additionValue = minigameManager.QuotaDeposit;
+        UIHelper.LerpAddFillImage(fillImage, additionValue, minigameManager.QuotaThreshold, quotaLerpValue);
+    }
+}
